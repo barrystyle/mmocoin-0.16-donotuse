ace9ca09bf00d4d682bc161c19b2aa95a1ad99d5
barrystyle <barrystyle@westnet.com.au>
2019-07-15 11:44:09 +0800

    switch pow algorithm for scrypt

--- src/init.cpp
+++ src/init.cpp
@@ -68,6 +68,10 @@
 #include <zmq/zmqnotificationinterface.h>
 #endif
 
+#ifdef USE_SSE2
+#include <crypto/scrypt.h>
+#endif
+
 static const bool DEFAULT_PROXYRANDOMIZE = true;
 static const bool DEFAULT_REST_ENABLE = false;
 static const bool DEFAULT_STOPAFTERBLOCKIMPORT = false;
@@ -1193,6 +1197,11 @@ bool AppInitMain()
     }
 
     int64_t nStart;
+
+#if defined(USE_SSE2)
+    std::string sse2detect = scrypt_detect_sse2();
+    LogPrintf("%s\n", sse2detect);
+#endif
 
     // ********************************************************* Step 5: verify wallet database integrity
 #ifdef ENABLE_WALLET
