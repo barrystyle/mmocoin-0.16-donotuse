50b9543935226b3f52683af00f6b92ad8050e764
Apple <admin@mmocoin.shop>
2019-08-23 00:04:36 +0500

    src: fix MAX MONEY typo in previous commit 35d5f9e

--- src/amount.h
+++ src/amount.h
@@ -29,7 +29,7 @@ static const std::string CURRENCY_UNIT = "MMO";
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 26000000 * COIN;
+static const CAmount MAX_MONEY = 260000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif //  BITCOIN_AMOUNT_H
